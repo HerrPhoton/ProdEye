57bf98933b8a052bd8d6a54529a6a9cf75ab8c58
Aleksandr Leisle <113707601+HerrPhoton@users.noreply.github.com>
2024-11-29 14:09:52 +0700

    Выбор сплита

--- scripts/duplicates_handler.py
+++ scripts/duplicates_handler.py
@@ -1,4 +1,5 @@
 import os
+from typing import Literal
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor
 
@@ -15,13 +16,14 @@ class DuplicatesHandler:
 
     def __init__(self,
                  dataset_path: str,
-                 dataset_type: str = "directory",
-                 similarity_threshold: float = 0.75,
+                 dataset_type: Literal["dir", "yolo"] = "dir",
+                 split: Literal["train", "valid", "test", "all"] = "all",
+                 similarity_threshold: float = 0.99,
                  yaml_filename: str = "data.yaml"):
         """
         Args:
             dataset_path: путь к датасету (абсолютный или относительный)
-            dataset_type: тип датасета ("directory" или "yolo")
+            dataset_type: тип датасета ("dir" или "yolo")
             similarity_threshold: порог схожести изображений (0-1)
             yaml_filename: имя yaml файла для YOLO датасета
         """
@@ -32,28 +34,28 @@ class DuplicatesHandler:
         self.dataset = None
         self.duplicates = None
 
-        if self.dataset_type not in ["directory", "yolo"]:
-            raise ValueError("dataset_type должен быть 'directory' или 'yolo'")
+        if self.dataset_type not in ["dir", "yolo"]:
+            raise ValueError("dataset_type должен быть 'dir' или 'yolo'")
 
-        self._load_dataset()
+        if split not in ["train", "valid", "test", "all"]:
+            raise ValueError("split должен быть 'train', 'valid', 'test' или 'all'")
 
-    def _load_dataset(self) -> None:
+        self._load_dataset(split)
+
+    def _load_dataset(self, split: Literal["train", "valid", "test", "all"]) -> None:
         """Загружает датасет в fiftyone."""
         if self.dataset_type == "yolo":
             self.dataset = fo.Dataset()
 
-            # Загружаем изображения из каждого сплита
-            splits = ['train', 'valid', 'test']
+            splits = ['train', 'valid', 'test'] if split == 'all' else [split]
             for split in splits:
                 split_images_path = os.path.join(self.dataset_path, split, 'images')
 
                 if os.path.exists(split_images_path):
-                    # Загружаем изображения из текущего сплита
-                    split_dataset = fo.Dataset.from_dir(
-                        dataset_dir=split_images_path,
-                        dataset_type=fo.types.ImageDirectory,
-                        tags=[split]  # Добавляем тег сплита
-                    )
+
+                    split_dataset = fo.Dataset.from_dir(dataset_dir=split_images_path,
+                                                        dataset_type=fo.types.ImageDirectory,
+                                                        tags=[split])
 
                     if len(split_dataset):
                         self.dataset.add_samples(split_dataset)
@@ -73,7 +75,6 @@ class DuplicatesHandler:
         self.dataset.add_sample_field("duplicate_group", fo.IntField, description="Индекс группы дубликатов")
 
         model = foz.load_zoo_model("mobilenet-v2-imagenet-torch")
-
         embeddings = self.dataset.compute_embeddings(model)
 
         if torch.cuda.is_available():
@@ -81,7 +82,6 @@ class DuplicatesHandler:
             similarity_matrix = torch.nn.functional.cosine_similarity(embeddings_tensor.unsqueeze(1),
                                                                       embeddings_tensor.unsqueeze(0),
                                                                       dim=2).cpu().numpy()
-
         else:
             with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
                 similarity_matrix = np.zeros((len(embeddings), len(embeddings)))
@@ -100,7 +100,6 @@ class DuplicatesHandler:
                     similarity_matrix[i] = similarities
 
         similarity_matrix = similarity_matrix - np.identity(len(similarity_matrix))
-
         id_map = [s.id for s in self.dataset.select_fields(["id"])]
 
         self.duplicates = {}
@@ -109,23 +108,24 @@ class DuplicatesHandler:
 
         for idx, sample in enumerate(tqdm(self.dataset.iter_samples(), total=len(self.dataset), desc="Поиск дубликатов")):
             if sample.filepath not in processed_files:
-
                 dup_idxs = np.where(similarity_matrix[idx] > self.similarity_threshold)[0]
 
                 if len(dup_idxs) > 0:
                     group = [sample.filepath]
                     processed_files.add(sample.filepath)
 
+                    sample.tags.append("has_duplicates")
                     sample.duplicate_group = current_group
                     sample.save()
 
                     for dup_idx in dup_idxs:
                         dup_sample = self.dataset[id_map[dup_idx]]
+
                         if dup_sample.filepath not in processed_files:
                             group.append(dup_sample.filepath)
                             processed_files.add(dup_sample.filepath)
 
-                            dup_sample.tags.extend(["duplicate", f"group_{current_group}"])
+                            dup_sample.tags.append("duplicate")
                             dup_sample.duplicate_group = current_group
                             dup_sample.save()
 
@@ -144,25 +144,8 @@ class DuplicatesHandler:
         if self.duplicates is None:
             raise ValueError("Сначала выполните поиск дубликатов методом find_duplicates()")
 
-        # Создаем представление с дубликатами и оригиналами
-        view = (
-            self.dataset.match_tags(["duplicate", "has_duplicates"]).sort_by("duplicate_group")  # Сортируем по группам
-        )
-
-        # Настраиваем отображение полей
-        view = view.select_fields(["tags", "duplicate_group", "filepath"])
-
-        # Запускаем приложение с настроенным отображением
-        session = fo.launch_app(view, auto=auto_launch)
-
-        # Настраиваем отображение полей в интерфейсе
-        session.view = view
-        session.refresh()
-
-        # Устанавливаем видимые поля
-        session.grid.grid_fields = ["tags", "duplicate_group"]
-
-        return session
+        view = self.dataset.match_tags(["has_duplicates", "duplicate"]).sort_by("duplicate_group")
+        fo.launch_app(self.dataset, view=view)
 
     def remove_duplicates(self, keep_first: bool = True) -> None:
         """Удаляет найденные дубликаты изображений.
