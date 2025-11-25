5a7ed69a155d98770ec8c4c55f499747ea628d7f
Aleksandr Leisle <113707601+HerrPhoton@users.noreply.github.com>
2025-11-25 15:28:00 +0700

    Обновлены докстринги

--- src/utils/extensions.py
+++ src/utils/extensions.py
@@ -6,6 +6,6 @@ def normalize_extensions(extensions: set[str]) -> set[str]:
     """Нормализует расширения, добавляя точку перед расширением, если она не добавлена
 
     :param set[str] extensions: Расширения для нормализации
-    :return set[str]: расширения
+    :return set[str]: Нормализованные расширения
     """
     return {ext if ext.startswith('.') else f'.{ext}' for ext in extensions}
