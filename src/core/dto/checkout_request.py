3c2d75081c9e9627213aa6070482345b87252c8f
Aleksandr Leisle <113707601+HerrPhoton@users.noreply.github.com>
2026-01-04 15:05:40 +0700

    Без артикула товара

--- src/core/dto/checkout_request.py
+++ src/core/dto/checkout_request.py
@@ -6,10 +6,7 @@ class CheckoutRequest:
     """
     Запрос от кассы на визуальную проверку товара.
 
-    :var sku: Артикул товара.
-    :vartype sku: str
     :var label: Название товара.
     :vartype label: str
     """
-    sku: str
     label: str
